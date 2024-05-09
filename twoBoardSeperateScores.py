69b124d8cb31dff069d842f1173b33300867d096
Leg0evan <99287638+Leg0evan@users.noreply.github.com>
2024-05-09 10:04:07 -0600

    Update twoBoardSeperateScores.py

--- twoBoardSeperateScores.py
+++ twoBoardSeperateScores.py
@@ -7,28 +7,50 @@ from time import sleep
 ###Imports RPi.GPIO
 import RPi.GPIO as GPIO
 
-###GPIO Pin Variable
+###GPIO Pin Variables
+
+#GPIO Pin One
 p1=6
+
+#GPIO Pin Two
 p2=5
 
 ###Setsup Board & GPIO Pin
+
+#Sets Board To BCM
 GPIO.setmode(GPIO.BCM)
+
+#Makes Pin One Input Pin
 GPIO.setup(p1, GPIO.IN)
+
+#Makes Pin Two Input Pin
 GPIO.setup(p2, GPIO.IN)
 
-###Makes Scoreboard Variables
+###Makes Scoreboard Variabless
+
+#Scoreboard Variable One
 scoreOne=Scores(26, 19, 13, 21, 20, 16, 12)
+
+#Scoreboard Variable Two
 scoreTwo=Scores(7, 8, 25, 24, 23, 18, 15)
 
-###Score Variable
+###Score Variables
+
+#Score For Scoreboard One
 x=0
+
+#Score For Scoreboard Two
 y=0
 
 ###Time Variable
 t=0
 
-###Button Press Variable
+###Inital Button Press Variables
+
+#Button One Intially Pressed Variable
 same1=False
+
+#Button Two Initially Pressed Variable
 same2=False
 
 ###Loop
@@ -45,13 +67,23 @@ while True:
                 same1=False
 
 ###Second Button Is Pressed
+
+#Checks For Initial Button Press
         if same2==False and GPIO.input(p2)==1:
+
+#Updates Score Of Button Two
                 y=y+1
                 scoreTwo.update(y)
+
+#Updates Initial Press Variable
                 same2=True
 
 ###Second Button Is Released
+
+#Checks When Button Two Is Released
         elif GPIO.input(p2)==0 and same2==True:
+
+#Resets Initial Butto Press Variable Of Button Two
                 same2=False
 
 ###Multiplexes Board (So it doesn't draw too much power)
